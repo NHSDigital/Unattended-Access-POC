897aa7db83a079b3f925591413ba7ecc9e855e4c
jalal <jalalhosseiny@gmail.com>
2020-06-25 12:24:45 +0100

    AMB-120 fix mock path

--- docker/unattended-access-poc/src/app.js
+++ docker/unattended-access-poc/src/app.js
@@ -1,7 +1,6 @@
 var ApiMocker = require('apimocker');
 var options = {};
-var path = require('path');
 
 ApiMocker.createServer(options)
-    .setConfigFile(path.join(__dirname, 'config.json'))
+    .setConfigFile('src/config.json')
     .start();
